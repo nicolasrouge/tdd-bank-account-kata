0969c316b41e9ae1297f2ad46dbe78266ee11ba9
Nicolas Rouge <nicolas.rouge777@gmail.com>
2023-12-31 16:43:21 +0000

    Add Guard Clauses

--- BankAccount/Models/BankAccount.cs
+++ BankAccount/Models/BankAccount.cs
@@ -11,35 +11,31 @@ namespace BankAccountKata.Models
 
         public void Deposit(decimal amount)
         {
-            if (amount > 0)
-            {
-                var deposit = transactionFactory.CreateDepositTransaction(amount, Balance);
-                Balance = deposit.Execute();
-                Transactions.Add(deposit);
-            }
-            else
+            if (amount <= 0)
             {
                 throw new ArgumentException("Deposit amount must be positive");
             }
+
+            var deposit = transactionFactory.CreateDepositTransaction(amount, Balance);
+            Balance = deposit.Execute();
+            Transactions.Add(deposit);
         }
 
         public void Withdraw(decimal amount)
         {
-            switch (amount)
+            if (amount <= 0)
             {
-                case <= 0:
-                    throw new ArgumentException("Withdrawal amount must be positive.");
-                case > 0 when Balance >= amount:
-                    {
-                        var withdraw = transactionFactory.CreateWithdrawTransaction(amount, Balance);
-                        Balance = withdraw.Execute();
-
-                        Transactions.Add(withdraw);
-                        break;
-                    }
-                default:
-                    throw new InvalidOperationException("Insufficient funds for the withdrawal.");
+                throw new ArgumentException("Withdrawal amount must be positive.");
             }
+
+            if (Balance < amount)
+            {
+                throw new InvalidOperationException("Insufficient funds for the withdrawal.");
+            }
+
+            var withdraw = transactionFactory.CreateWithdrawTransaction(amount, Balance);
+            Balance = withdraw.Execute();
+            Transactions.Add(withdraw);
         }
     }
 }
